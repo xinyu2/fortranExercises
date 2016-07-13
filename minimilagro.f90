39eb18e1375066856720746671e8c1b978f80b2a
Xinyu Chen <Xinyu@pn1615996.lanl.gov>
2016-07-13 17:08:01 -0600

    scatter particle

--- minimilagro.f90
+++ minimilagro.f90
@@ -7,20 +7,27 @@ program minimilagro
 ! TODO and wishlist:
 !***********************************************************************
   integer,parameter :: impi0=0 !the master rank
-  integer,parameter :: maxpars=640,totalValue=0
+  integer,parameter :: maxpars=17,totalValue=0
   integer,parameter :: dimx=4,dimy=8,dimz=1
   logical :: lmpi0 !true for the master rank
   integer :: impi !mpi rank
   integer :: nmpi !number of mpi tasks
   integer :: ierr,it
-  integer :: tsp_start,tsp_end
+  integer :: tsp_start,tsp_end !time step
+  integer :: dsize !domain size(total cells=dimx*dimy*dimz)
+  integer :: rsize !rank size(cells per rank=dimx*dimy*dimz/nmpi)
   type par
      integer :: x, y, z  ! coordinate
      integer :: dx,dy,dz ! delta coordinate
-     real*8  :: energy   ! positive or negative
-     logical :: cmplt    ! flag for complete
+     integer :: r        ! mpi rank
+     real*8  :: e        ! positive or negative energy
   end type par
   type(par),allocatable,target :: pars(:)
+  type cell
+     integer :: gid  ! global id
+     integer :: rid  ! rank id
+  end type cell
+  type(cell),dimension(:),allocatable,target :: dd ! reordered cell index
 !
 !-- mpi initialization
   call mpi_init(ierr) !MPI
@@ -28,7 +35,7 @@ program minimilagro
   call mpi_comm_size(MPI_COMM_WORLD,nmpi,ierr) !MPI
   lmpi0 = impi==impi0
   tsp_start = 1
-  tsp_end   = 40
+  tsp_end   = 4
 !
 !-- read and distribut input data
 !================================
@@ -36,6 +43,8 @@ program minimilagro
 !
   if(lmpi0) then
      call initParticles(nmpi,maxpars,pars)
+     call domainDecompose(nmpi,dd,dsize,rsize)
+     call scatterParticles(nmpi,maxpars,pars,dd)
      do it=tsp_start,tsp_end
 
      enddo !tsp_it
@@ -55,34 +64,109 @@ program minimilagro
   call mpi_finalize(ierr) !MPI
 
 contains
-  subroutine initParticles(mpi_size,par_size,out_pars)
+  subroutine initParticles(mpi_size,size,pars)
     implicit none
-    integer,intent(in)::mpi_size,par_size
-    type(par),dimension(:),allocatable,intent(out)::out_pars
+    integer,intent(in)::mpi_size,size
+    type(par),dimension(:),allocatable,intent(out)::pars
     integer :: i
     real*8  :: temp,tot ! use tot to check sum of energy is zero
-    allocate(out_pars(par_size))
+    allocate(pars(size))
     tot=0
     call random_seed()
-    do i=1,par_size
-      call random_number(temp)
-      out_pars(i)%x=int(temp*dimx)+1
-      call random_number(temp)
-      out_pars(i)%y=int(temp*dimy)+1
-      call random_number(temp)
-      out_pars(i)%z=int(temp*dimz)+1
-      if (i<par_size) then
-         call random_number(temp)
-         out_pars(i)%energy=temp*2-1
-         tot=tot+out_pars(i)%energy
-      else
-         out_pars(i)%energy=0-tot
-         tot=tot+out_pars(i)%energy
-         write(6,*) '>',i,'energy=',out_pars(i)%energy
+    do i=1,size
+      pars(i)%x=getCoor(dimx)
+      pars(i)%y=getCoor(dimy)
+      pars(i)%z=getCoor(dimz)
+      if(i<=size/2) then
+         temp=getEnergy()
+         pars(i)%e=temp
+         pars(size-i+1)%e=-1*temp
       endif
+      if (mod(size,2)==0) then
+      else
+         if(i==size/2+1) then
+            pars(i)%e=0
+         endif
+      endif
+      tot=tot+pars(i)%e
+      write(6,*) '->',pars(i)%x,pars(i)%y,pars(i)%z,pars(i)%e
     enddo
     write(6,*) '>>',tot
   end subroutine initParticles
+
+  subroutine domainDecompose(mpi_size,dd,v,c)
+    implicit none
+    integer,intent(in)::mpi_size
+    type(cell),dimension(:),intent(out),allocatable :: dd
+    integer,intent(out) :: v ! volume, domain size, number of cells
+    integer,intent(out) :: c ! cells per rank
+    integer :: i,j,k,idx
+    v = dimx*dimy*dimz
+    c = v/mpi_size
+    allocate(dd(v))
+    !**********************
+    ! strip decomposition
+    !**********************
+    do i=1,v
+       dd(i)%gid=i
+       dd(i)%rid=(i-1)/c
+    enddo
+  end subroutine domainDecompose
+
+  subroutine scatterParticles(mpi_size,psize,pars,dd)
+    implicit none
+    integer,intent(in)::mpi_size
+    integer,intent(in)::psize ! particle size
+    type(par),dimension(:),intent(inout)::pars
+    type(cell),dimension(:),intent(in) :: dd
+    integer,dimension(:),allocatable :: counts,displ,offset
+    type(par),dimension(:),allocatable::scatter
+    integer :: i,paridx,rankid,pointer
+    allocate(counts(mpi_size),displ(mpi_size),offset(mpi_size))
+    allocate(scatter(psize))
+    do i=1,mpi_size
+       counts(i)=0
+       displ(i)=0
+       offset(i)=0
+    enddo
+    do i=1,psize
+       paridx=pars(i)%x+(pars(i)%y-1)*dimx
+       pars(i)%r=dd(paridx)%rid
+       counts(dd(paridx)%rid+1)=counts(dd(paridx)%rid+1)+1
+    enddo
+    displ(1)=1
+    do i=2,mpi_size
+       displ(i)=counts(i-1)+displ(i-1)
+    enddo
+    do i=1,psize
+       rankid=pars(i)%r
+       pointer=displ(rankid+1)+offset(rankid+1)
+       offset(rankid+1)=offset(rankid+1)+1
+       scatter(pointer)=pars(i)
+    enddo
+    do i=1,psize
+       write(6,*) '>',i,scatter(i)%r
+    enddo
+
+  end subroutine scatterParticles
+
+  integer function getCoor(dim)
+    implicit none
+    integer,intent(in)::dim
+    integer :: c
+    real*8  :: temp
+    call random_number(temp)
+    c=int(temp*dim)+1
+    getCoor=c
+  end function getCoor
+
+  real*8 function getEnergy()
+    implicit none
+    real*8  :: temp, e
+    call random_number(temp)
+    e=temp*2-1
+    getEnergy=e
+  end function getEnergy
 
 end program minimilagro
 ! vim: fdm=marker
